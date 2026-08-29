8a4876667ce78a8ea87bb588c53addb0040c20b1
hicham <hichambeloualed@gmail.com>
2026-08-29 11:36:06 +0100

    feat: create a notFound page

---
+++ FRONTEND/src/pages/notFound.jsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className="flex justify-center items-center text-4xl min-h-screen flex-col gap-5 text-white bg-bg">
+      <h1>404</h1>
+      <p>Page not found</p>
+    </div>
+  );
+}
